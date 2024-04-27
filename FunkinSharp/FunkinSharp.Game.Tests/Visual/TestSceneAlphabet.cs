a82f3c3a117265444f1315988608c855f23bdfe2
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2024-04-27 18:43:59 +0200

    WIP Alphabet implementation.

---
+++ FunkinSharp/FunkinSharp.Game.Tests/Visual/TestSceneAlphabet.cs
@@ -0,0 +1,48 @@
+﻿using FunkinSharp.Game.Funkin.Text;
+using NUnit.Framework;
+using osu.Framework.Graphics.Containers;
+using osuTK;
+
+namespace FunkinSharp.Game.Tests.Visual
+{
+    [TestFixture]
+    public partial class TestSceneAlphabet : FunkinSharpTestScene
+    {
+        private Container<AlphaCharacter> alphabet;
+
+        public TestSceneAlphabet()
+        {
+            alphabet = [];
+            string str = "Hello World!";
+            bool lastSpace = false;
+
+            foreach (char character in str.ToCharArray())
+            {
+                if (character.ToString() == " ")
+                {
+                    lastSpace = true;
+                    continue;
+                }
+
+                Vector2 pos = (lastSpace) ? new Vector2(40, 0) : Vector2.Zero;
+                AlphaCharacter letter = new AlphaCharacter(pos, character.ToString(), true);
+                letter.OnLoadComplete += letter_OnLoadComplete;
+                letter.Origin = osu.Framework.Graphics.Anchor.TopLeft;
+                letter.Anchor = osu.Framework.Graphics.Anchor.TopLeft;
+                alphabet.Add(letter);
+                lastSpace = false;
+            }
+            Add(alphabet);
+        }
+
+        private void letter_OnLoadComplete(osu.Framework.Graphics.Drawable obj)
+        {
+            AlphaCharacter character = (AlphaCharacter)obj;
+            if (alphabet.IndexOf(character) != 0)
+            {
+                AlphaCharacter prevChar = alphabet[alphabet.IndexOf(character) - 1];
+                character.X += prevChar.X + prevChar.DrawWidth;
+            }
+        }
+    }
+}
---
+++ FunkinSharp/FunkinSharp.Game/Funkin/Text/AlphaCharacter.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using FunkinSharp.Game.Core;
+using FunkinSharp.Game.Core.Stores;
+using osu.Framework.Allocation;
+using osu.Framework.Logging;
+using osuTK;
+
+namespace FunkinSharp.Game.Funkin.Text
+{
+    // Base Game 0.2.7.1 mixed with some Psych code
+    // Might rewrite it a little bit
+    public partial class AlphaCharacter : FrameAnimatedSprite
+    {
+        // Only contains the necessary definitions
+        public static readonly Dictionary<string, LetterDefinition> LETTERS = new()
+        {
+            // Symbols
+            { "&", new LetterDefinition("amp", Vector2.Zero, new Vector2(0, 2)) },
+            { "(", new LetterDefinition(string.Empty, Vector2.Zero, new Vector2(0, 5)) },
+            { ")", new LetterDefinition(string.Empty, Vector2.Zero, new Vector2(0, 5)) },
+            { "*", new LetterDefinition(string.Empty, new Vector2(0, 28), Vector2.Zero) },
+
+            { "+", new LetterDefinition(string.Empty, new Vector2(0, 7), new Vector2(0, -12)) },
+            { "-", new LetterDefinition(string.Empty, new Vector2(0, 16), new Vector2(0, -30)) },
+            { "<", new LetterDefinition("lt", Vector2.Zero, new Vector2(0, 4)) },
+            { ">", new LetterDefinition("gt", Vector2.Zero, new Vector2(0, 4)) },
+
+            { "'", new LetterDefinition("apostrophe", new Vector2(0, 32), Vector2.Zero) },
+            { "\"", new LetterDefinition("quote", new Vector2(0, 32), Vector2.Zero) },
+            { "!", new LetterDefinition("exclamation", Vector2.Zero, new Vector2(0, 10)) },
+            { "?", new LetterDefinition("question", Vector2.Zero, new Vector2(0, 4)) },
+
+            { ".", new LetterDefinition("period", Vector2.Zero, new Vector2(0, -44)) },
+            { "❝", new LetterDefinition("start quote", new Vector2(0, 24), new Vector2(0, -5)) },
+            { "❞", new LetterDefinition("end quote", new Vector2(0, 24), new Vector2(0, -5)) },
+
+            // No bold
+            { "#", new LetterDefinition("hashtag", Vector2.Zero, Vector2.Zero, false) },
+            { "$", new LetterDefinition("dollarsign", Vector2.Zero, Vector2.Zero, false) },
+
+            { ":", new LetterDefinition(string.Empty, new Vector2(0, 2), Vector2.Zero, false) },
+            { ";", new LetterDefinition(string.Empty, new Vector2(0, -2), Vector2.Zero, false) },
+            { "]", new LetterDefinition(string.Empty, new Vector2(0, -1), Vector2.Zero, false) },
+            { "^", new LetterDefinition(string.Empty, new Vector2(0, 28), Vector2.Zero, false) },
+
+            { ",", new LetterDefinition("comma", new Vector2(0, -6), Vector2.Zero, false) },
+            { "\\", new LetterDefinition("back slash", Vector2.Zero, Vector2.Zero, false) },
+            { "/", new LetterDefinition("forward slash", Vector2.Zero, Vector2.Zero, false) },
+            { "~", new LetterDefinition(string.Empty, new Vector2(0, 16), Vector2.Zero, false) },
+
+        };
+
+        public const double FRAME_DURATION = 48; // Increase the duration of the frames so it looks better?
+
+        public readonly string Letter = "?";
+        public bool IsBold { get; private set; } = false;
+
+        public Vector2 LetterOffset { get; private set; } = Vector2.Zero;
+
+        // We set the letter info on creation since the animation is setup on Load which is async and trying to run "CreateBold" results on nothing since the sprite didnt load yet
+        public AlphaCharacter(Vector2 position, string letter, bool isBold = false)
+        {
+            Position = position;
+            Letter = letter;
+            IsBold = isBold;
+            Loop = true;
+        }
+
+        // Somehow it works but I think it's not gonna last long lmao
+        [BackgroundDependencyLoader]
+        private void load(SparrowAtlasStore sparrowStore)
+        {
+            Atlas = sparrowStore.GetSparrow("Textures/alphabet");
+
+            LetterDefinition def = LETTERS["?"];
+            string lowercased = Letter.ToLower();
+            if (LETTERS.ContainsKey(lowercased))
+                def = LETTERS[lowercased];
+
+            string suffix;
+            if (IsBold)
+            {
+                suffix = " bold";
+                if (def.BoldOffset != Vector2.Zero)
+                    LetterOffset = def.BoldOffset;
+            }
+            else
+            {
+                if (isTypeAlphabet(lowercased[0]))
+                    suffix = (lowercased != Letter) ? " uppercase" : " lowercase";
+                else
+                {
+                    suffix = " normal";
+                    if (def.Offset != Vector2.Zero)
+                        LetterOffset = def.Offset;
+                }
+            }
+
+            string animation = lowercased;
+            if (!Equals(def, LETTERS["?"]) && def.Animation != string.Empty)
+                animation = def.Animation;
+
+            if (suffix == " bold" && !def.HasBold)
+            {
+                suffix = " normal";
+                IsBold = false;
+            }
+
+            string key = $"{animation}{suffix}";
+            if (Animations.TryGetValue(key, out AnimationFrame anim))
+            {
+                AddFrameRange(anim.StartFrame, anim.EndFrame, FRAME_DURATION);
+                CurAnim = anim;
+                CurAnimName = key;
+            }
+            else
+            {
+                if (suffix != " bold")
+                    suffix = " normal";
+
+                key = $"question{suffix}";
+
+                AnimationFrame qanim = Animations[key];
+                AddFrameRange(qanim.StartFrame, qanim.EndFrame, FRAME_DURATION);
+                CurAnim = qanim;
+                CurAnimName = key;
+
+                Logger.Log($"Missing Letter ({Letter}/{animation}){suffix} Animation", level: LogLevel.Error);
+            }
+        }
+
+        private bool isTypeAlphabet(char c)
+        {
+            return (c >= 65 && c <= 90) || (c >= 97 && c <= 122);
+        }
+    }
+}
