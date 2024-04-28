eddec101bfdee79ab97b787b625346f3c6f62e67
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2024-04-28 11:46:53 +0200

    Receptor implementation including JSON soft modding for the data.

---
+++ FunkinSharp/FunkinSharp.Game.Tests/Visual/TestSceneReceptor.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using FunkinSharp.Game.Core.Stores;
+using FunkinSharp.Game.Funkin.Compat;
+using FunkinSharp.Game.Funkin.Notes;
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Testing.Drawables.Steps;
+
+namespace FunkinSharp.Game.Tests.Visual
+{
+    [TestFixture]
+    public partial class TestSceneReceptor : FunkinSharpTestScene
+    {
+        private List<StepButton> anims = [];
+        private FEReceptorData data;
+        private string lastAnim = "static";
+
+        private Receptor receptor;
+        private string noteType = "default";
+        private int noteData = 0;
+
+        public TestSceneReceptor()
+        {
+            AddSliderStep("Receptor Note Data", 0, 3, 0, (v) =>
+            {
+                if (noteData == v || receptor == null) return;
+
+                noteData = v;
+                Remove(receptor, true);
+                clean();
+
+                receptor = new Receptor(data, noteData, noteType);
+                receptor.OnLoadComplete += receptor_OnLoadComplete;
+                Add(receptor);
+            });
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(JSONStore jsonStore)
+        {
+            data = jsonStore.Get<FEReceptorData>($"NoteTypes/{noteType}/{noteType}");
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            receptor = new Receptor(data, noteData, noteType);
+            receptor.OnLoadComplete += receptor_OnLoadComplete;
+            Add(receptor);
+        }
+
+        private void receptor_OnLoadComplete(osu.Framework.Graphics.Drawable obj)
+        {
+            foreach (var alias in receptor.Aliases)
+            {
+                StepButton step = AddStep($"Play {alias.Key}", () =>
+                {
+                    receptor.Play(alias.Key);
+                    lastAnim = alias.Key;
+                });
+
+                if (lastAnim == alias.Key)
+                {
+                    step.TriggerClick();
+                }
+
+                anims.Add(step);
+            }
+
+            AddToggleStep("Loop Animation", (state) =>
+            {
+                receptor.Loop = state;
+            });
+        }
+
+        private void clean()
+        {
+            foreach (StepButton step in anims)
+            {
+                StepsContainer.Remove(step, true);
+            }
+
+            StepsContainer.Remove(StepsContainer[^1], true);
+            anims = [];
+        }
+    }
+}
---
+++ FunkinSharp/FunkinSharp.Game/Funkin/Notes/Receptor.cs
@@ -0,0 +1,96 @@
+﻿using osu.Framework.Graphics;
+using FunkinSharp.Game.Core;
+using FunkinSharp.Game.Funkin.Compat;
+using System.Collections.Generic;
+using osu.Framework.Logging;
+using osu.Framework.Allocation;
+using FunkinSharp.Game.Core.Stores;
+using osu.Framework.Graphics.Textures;
+using osuTK;
+
+namespace FunkinSharp.Game.Funkin.Notes
+{
+    // Legacy code but wit JSON stuff hehe
+    // TODO: In order to add soft modding (?) I would need to add some basic interp or sum shit
+    // I'm thinking about Wren or Luau?¿ probably wren
+    // https://github.com/SanicBTW/Just-Another-FNF-Engine/blob/master/source/funkin/notes/Receptor.hx
+    public partial class Receptor : FrameAnimatedSprite
+    {
+        public Dictionary<string, string> Aliases { get; private set; } = []; // Holds the aliases of the Sparrow Animations
+
+        public float SwagWidth { get; private set; }
+
+        public readonly int NoteData;
+        public readonly string NoteType;
+
+        public FEReceptorData ReceptorData { get; private set; }
+        // public WrenModule NoteModule { get; private set; } - soon hehe
+
+        public float InitialX;
+        public float InitialY;
+
+        public float SetAlpha = 0.8f;
+
+        public float HoldTimer = 0;
+        public float Direction = 90;
+
+        public Receptor(FEReceptorData receptorData, int noteData = 0, string noteType = "default")
+        {
+            ReceptorData = receptorData;
+            NoteData = noteData;
+            NoteType = noteType;
+
+            Anchor = Origin = Anchor.Centre;
+            SwagWidth = receptorData.Separation * receptorData.Size;
+        }
+
+        public override void Play(string animName, bool Force = true)
+        {
+            if (Aliases.TryGetValue(animName, out string realAnim) && CanPlayAnimation(Force))
+            {
+                IsFinished = false;
+                CurFrame = 0;
+                CurAnimName = animName;
+
+                AnimationFrame newAnim = Animations[realAnim];
+                GotoFrame(newAnim.StartFrame);
+                CurAnim = newAnim;
+
+                Alpha = (animName == "confirm") ? 1 : SetAlpha;
+            }
+
+            if (!Aliases.ContainsKey(animName))
+            {
+                Logger.Log($"Animation Alias ({animName}) not found on Receptor {NoteData} with skin {NoteType}", level: LogLevel.Error);
+                base.Play(animName, Force);
+            }
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(SparrowAtlasStore sparrowStore)
+        {
+            Atlas = sparrowStore.GetSparrow($"NoteTypes/{NoteType}/{ReceptorData.Texture}");
+            foreach (Texture frame in Atlas.Frames)
+            {
+                AddFrame(frame, 24);
+            }
+
+            string stringSect = GetNoteDirection();
+            Aliases["static"] = $"arrow{stringSect.ToUpper()}";
+            Aliases["pressed"] = $"{stringSect} press";
+            Aliases["confirm"] = $"{stringSect} confirm";
+            Scale = new Vector2(ReceptorData.Size);
+            Play("static");
+        }
+
+        public string GetNoteDirection()
+        {
+            return ReceptorData.Actions[NoteData];
+        }
+
+        public string GetNoteColor()
+        {
+            return ReceptorData.Colors[NoteData];
+        }
+    }
+}
