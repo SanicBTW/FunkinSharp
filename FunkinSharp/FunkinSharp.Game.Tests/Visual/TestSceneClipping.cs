cde0ad7b89ddf549fc40731ee5459a8f54273be9
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2024-04-28 13:53:09 +0200

    Clipping/Masking testing and a new container dedicated to that.

---
+++ FunkinSharp/FunkinSharp.Game.Tests/Visual/TestSceneClipping.cs
@@ -0,0 +1,94 @@
+﻿using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics;
+using osuTK.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osuTK;
+using osu.Framework.Graphics.Containers;
+using FunkinSharp.Game.Funkin.Sprites;
+using FunkinSharp.Game.Core;
+
+namespace FunkinSharp.Game.Tests.Visual
+{
+    public partial class TestSceneClipping : FunkinSharpTestScene
+    {
+        public TestSceneClipping()
+        {
+            Character bf = new Character("bf", true)
+            {
+                X = 100,
+                Scale = new Vector2(1.5f)
+            };
+
+            ClippedContainer clipp = new ClippedContainer
+            {
+                Width = 0.55f,
+                Height = 0.25f
+            };
+
+            clipp.Add(bf);
+
+            Children = new Drawable[]
+            {
+                clipp,
+                new Container
+                {
+                    Name = "Info Overlay",
+                    RelativeSizeAxes = Axes.Both,
+                    Masking = true,
+                    BorderColour = Color4.Red,
+                    BorderThickness = 4,
+                    Children = new Drawable[]
+                    {
+                        new Box
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Alpha = 0,
+                            AlwaysPresent = true,
+                            Colour = Colour4.White
+                        },
+                        new SpriteText
+                        {
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.TopCentre,
+                            Text = "Out of bounds",
+                            Colour = Color4.Red,
+                            Font = FontUsage.Default.With(size: 36)
+                        },
+                        new Container
+                        {
+                            Anchor = Anchor.Centre,
+                            Origin = Anchor.Centre,
+                            RelativeSizeAxes = Axes.Both,
+                            Size = clipp.Size,
+                            Masking = true,
+                            BorderColour = Color4.Green,
+                            BorderThickness = 4,
+                            Children = new Drawable[]
+                            {
+                                new Box
+                                {
+                                    RelativeSizeAxes = Axes.Both,
+                                    Alpha = 0,
+                                    AlwaysPresent = true
+                                },
+                                new SpriteText
+                                {
+                                    Anchor = Anchor.TopCentre,
+                                    Origin = Anchor.TopCentre,
+                                    Text = "Masked area",
+                                    Colour = Color4.Green,
+                                    Font = FontUsage.Default.With(size: 36)
+                                }
+                            }
+                        }
+                    }
+                },
+            };
+
+            AddStep("Play", () =>
+            {
+                bf.Play("idle", true);
+            });
+        }
+    }
+}
---
+++ FunkinSharp/FunkinSharp.Game/Core/ClippedContainer.cs
@@ -0,0 +1,35 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+
+namespace FunkinSharp.Game.Core
+{
+    // Basic container made for clipping the children inside of it
+    // It's recommended that the sprites that will be added doesn't have the relative size axes set
+    // Also it's recommended to set width and height manually instead of Size directly (too much math!!)
+
+    // TODO: Debug overlay??
+    public partial class ClippedContainer : Container
+    {
+        protected override Container<Drawable> Content => ClippedContent;
+
+        protected readonly Container ClippedContent;
+
+        public ClippedContainer()
+        {
+            Name = "Clipper Mask";
+            Anchor = Anchor.Centre;
+            Origin = Anchor.Centre;
+            RelativeSizeAxes = Axes.Both;
+            Masking = true;
+
+            AddInternal(ClippedContent = new Container
+            {
+                Name = "Clipped Content",
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                RelativeSizeAxes = Axes.Both,
+                Masking = true
+            });
+        }
+    }
+}
