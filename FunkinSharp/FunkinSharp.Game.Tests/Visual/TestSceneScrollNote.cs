429db37ba9d668c97c4cc9bec3f843e0a82f04ff
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2024-04-28 15:56:32 +0200

    Basic Scroll Note implementation.

---
+++ FunkinSharp/FunkinSharp.Game.Tests/Visual/TestSceneScrollNote.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using FunkinSharp.Game.Core.Stores;
+using FunkinSharp.Game.Funkin.Compat;
+using FunkinSharp.Game.Funkin.Notes;
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Testing.Drawables.Steps;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FunkinSharp.Game.Tests.Visual
+{
+    [TestFixture]
+    public partial class TestSceneScrollNote : FunkinSharpTestScene
+    {
+        private Note note;
+        private string noteType = "default";
+        private int noteData = 0;
+
+        public TestSceneScrollNote()
+        {
+            AddSliderStep("Note Data", 0, 3, 0, (v) =>
+            {
+                if (noteData == v || note == null) return;
+
+                noteData = v;
+                Remove(note, true);
+
+                note = new Note(0, noteData, noteType)
+                {
+                    Y = 0
+                };
+                Add(note);
+            });
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            note = new Note(0, noteData, noteType)
+            {
+                Y = 0
+            };
+            Add(note);
+        }
+    }
+}
---
+++ FunkinSharp/FunkinSharp.Game/Funkin/Notes/Note.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using FunkinSharp.Game.Core;
+using FunkinSharp.Game.Core.Stores;
+using FunkinSharp.Game.Funkin.Compat;
+using osu.Framework.Allocation;
+using osuTK;
+
+namespace FunkinSharp.Game.Funkin.Notes
+{
+    // Scroll Note, shouldn't have any other animation aside from the <color>Scroll one, so its going to be just like AlphaCharacter
+    // This will have the Sustain attached to it but won't hold any logic for drawing or loading the frames
+    // Some stuff is based of Forever Engine : Rewrite and my shi https://github.com/SanicBTW/Just-Another-FNF-Engine/blob/master/source/funkin/notes/Note.hx
+    public partial class Note : FrameAnimatedSprite
+    {
+        public static Dictionary<string, FEReceptorData> DataCache = [];
+
+        public readonly float StrumTime;
+        public readonly int NoteData;
+        public readonly string NoteType;
+
+        public FEReceptorData ReceptorData { get; private set; }
+
+        public Note(float strumTime, int noteData, string noteType = "default")
+        {
+            StrumTime = strumTime;
+            NoteData = noteData;
+            NoteType = noteType;
+            Loop = true;
+            Anchor = Origin = osu.Framework.Graphics.Anchor.Centre;
+
+            Y -= 2000;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(JSONStore jsonStore, SparrowAtlasStore sparrowStore)
+        {
+            if (NoteData < -1)
+                return;
+
+            // these bad boys should be already cached but we ball anyways
+            if (DataCache.ContainsKey(NoteType))
+                ReceptorData = DataCache[NoteType];
+            else
+                ReceptorData = DataCache[NoteType] = jsonStore.Get<FEReceptorData>($"NoteTypes/{NoteType}/{NoteType}");
+
+            Atlas = sparrowStore.GetSparrow($"NoteTypes/{NoteType}/{ReceptorData.Texture}");
+
+            // AlphaCharacter stuff, basically add only the frames inside the range
+            if (Animations.TryGetValue(GetNoteColor(), out AnimationFrame anim))
+            {
+                AddFrameRange(anim.StartFrame, anim.EndFrame);
+                CurAnim = anim;
+                CurAnimName = GetNoteColor();
+                Scale = new Vector2(ReceptorData.Size);
+            }
+        }
+
+        public string GetNoteDirection()
+        {
+            return ReceptorData.Actions[NoteData];
+        }
+
+        public string GetNoteColor()
+        {
+            return ReceptorData.Colors[NoteData];
+        }
+    }
+}
