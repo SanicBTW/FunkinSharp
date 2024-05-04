9d43707f8169dabf68f5ac9ac235914e0cbe1bd7
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2024-05-04 20:23:44 +0200

    Added Note Kind.

--- FunkinSharp/FunkinSharp.Game/Funkin/Song/SongNoteData.cs
+++ FunkinSharp/FunkinSharp.Game/Funkin/Song/SongNoteData.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.ComponentModel;
+using Newtonsoft.Json;
 
 namespace FunkinSharp.Game.Funkin.Song
 {
@@ -6,6 +7,7 @@ namespace FunkinSharp.Game.Funkin.Song
     // ok yeah they are steps but just in case imma use strum time cuz why not
     // both time and length are step time :sob:
     // decompiled mentions note "kind" but in charts theres nothing
+    // nvm i found a chart that uses kind, basically is the note type shi from psych and yknow the drill
     public class SongNoteData
     {
         [JsonProperty("t")]
@@ -17,11 +19,16 @@ namespace FunkinSharp.Game.Funkin.Song
         [JsonProperty("l")]
         public readonly float Length;
 
-        public SongNoteData(float time, int data, float length)
+        [JsonProperty("k")]
+        [DefaultValue("normal")]
+        public readonly string Kind;
+
+        public SongNoteData(float time, int data, float length, string kind = "normal")
         {
             Time = time;
             Data = data;
             Length = length;
+            Kind = kind;
         }
     }
 }
