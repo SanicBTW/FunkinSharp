d7a866627c887dea65b470bfc5a339d196ae9358
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2024-05-12 14:26:41 +0200

    Reconsider the usage of Paths.

--- FunkinSharp/FunkinSharp.Game/Core/Paths.cs
+++ FunkinSharp/FunkinSharp.Game/Core/Paths.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 using FunkinSharp.Game.Core.Sparrow;
@@ -13,16 +12,15 @@ using osu.Framework.Platform;
 
 namespace FunkinSharp.Game.Core
 {
+    // Currently Paths are used to load assets whenever you want instead of waiting the load function to run
+    // In a near future, paths will resolve paths for external assets or allow zips to be dropped to run mods n shi
     // TODO: Add proper caching
     public static class Paths
     {
-        // Cache stuff, tries to dispose most of the stuff when it cans
+        // Cache stuff, tries to dispose most of the stuff when its able to do it
         private static Dictionary<string, Texture> keyedTextures = new Dictionary<string, Texture>();
         private static Dictionary<string, SparrowAtlas> keyedAtlases = new Dictionary<string, SparrowAtlas>();
         private static Dictionary<string, Track> keyedTracks = new Dictionary<string, Track>();
-
-        private static NativeStorage cwdStorage; // Current Working Directory Storage, refers to the current path where the executable is
-        private static NativeStorage assetsStorage; // Refers to the asset folder created
 
         // Needed game instances to be able to build usable objects in game
 
@@ -38,30 +36,18 @@ namespace FunkinSharp.Game.Core
             game_host = host;
             audio_manager = audioManager;
             dllResources = resources;
-
-            cwdStorage = new NativeStorage(Path.GetDirectoryName(host.FullPath), host);
-            assetsStorage = (NativeStorage)cwdStorage.GetStorageForDirectory("assets");
-            createPaths();
         }
 
-        // Creates the same folder tree schema from FNF 0.3 or standard HaxeFlixel
-        private static void createPaths()
-        {
-            // We need assets storage created before this (?)
+        /*
+         * Now it only focuses on DLL Resources, i'm currently thinking on some idea of adding a system
+         * where Paths asks some system (Plugins or Events) to resolve the provided path for it
+         * since the current target of the engine is to only provide the full rhythm game experience
+         * modding may come on a later stage but since i'm currently unable to mess with paths correctly
+         * im not going to add ANY support for external assets
+         */
 
-        }
-
-        // We will always look for the asset inside the game resources first
-        // or by checking the path, knowing we gotta either use game resources or native storage
-        // since most of the paths provided in game doesnt start with assets or a full path
-
-        // TODO: Be able to pass more arguments for the texture generation
         public static SparrowAtlas GetSparrow(string path)
         {
-            // Save the check before appending the missing extension (?)
-            string assetPath = cwdStorage.GetFullPath("assets");
-            bool isFromNative = path.StartsWith(assetPath);
-
             if (!path.EndsWith(".xml"))
                 path += ".xml";
 
@@ -69,27 +55,13 @@ namespace FunkinSharp.Game.Core
             if (catlas != null)
                 return catlas;
 
-            XmlReader xmlReader;
-
-            // Provided path is from NativeStorage
-            if (isFromNative)
-            {
-                // TODO: Proper fallbacking?
-                xmlReader = XmlReader.Create(assetsStorage.GetStream(path));
-            }
-            else // Provided path is from DLL Resources
-            {
-                xmlReader = XmlReader.Create(dllResources.GetStream(path));
-            }
-
+            using XmlReader xmlReader = XmlReader.Create(dllResources.GetStream(path));
             SparrowAtlas atlas = AssetFactory.ParseSparrow(xmlReader);
 
-            // overhaul this shi bru
+            // rewrite this shi bru
             Texture endTexture;
 
-            string imagePath = isFromNative ?
-                Path.Join(Path.GetDirectoryName(path) ?? "", atlas.TextureName) :
-                SanitizeForResources($"{Path.GetDirectoryName(path) ?? ""}/{atlas.TextureName}");
+            string imagePath = SanitizeForResources($"{Path.GetDirectoryName(path) ?? ""}/{atlas.TextureName}");
 
             Cache(atlas.TextureName, out Texture ctexture);
             if (ctexture != null)
@@ -104,16 +76,18 @@ namespace FunkinSharp.Game.Core
 
         public static Track GetTrack(string path)
         {
-            throw new NotImplementedException();
+            Track newTrack = AssetFactory.CreateTrack(dllResources.GetStream(path), path);
+            Cache(path, newTrack);
+            audio_manager.TrackMixer.Add(newTrack);
+            audio_manager.AddItem(newTrack);
+            return newTrack;
         }
 
-        // TODO: improve this since a full proper path is expected
         public static Texture GetTexture(string path)
         {
-            string assetPath = cwdStorage.GetFullPath("assets");
-            bool isFromNative = path.StartsWith(assetPath);
-
-            return AssetFactory.CreateTexture(renderer, (isFromNative) ? assetsStorage.GetStream(path) : dllResources.GetStream(path), TextureFilteringMode.Linear, WrapMode.ClampToEdge, WrapMode.ClampToEdge, true);
+            Texture newTexture = AssetFactory.CreateTexture(renderer, dllResources.GetStream(path), TextureFilteringMode.Linear, WrapMode.ClampToEdge, WrapMode.ClampToEdge, true);
+            Cache(path, newTexture);
+            return newTexture;
         }
 
         // replaces '\' and the directory separator character with '/'
@@ -129,6 +103,5 @@ namespace FunkinSharp.Game.Core
         public static void Cache(string key, out Texture cached) => cached = keyedTextures.ContainsKey(key) ? keyedTextures[key] : null;
         public static void Cache(string key, out SparrowAtlas cached) => cached = keyedAtlases.ContainsKey(key) ? keyedAtlases[key] : null;
         public static void Cache(string key, out Track cached) => cached = keyedTracks.ContainsKey(key) ? keyedTracks[key] : null;
-
     }
 }
